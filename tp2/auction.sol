699acd5244d0801955b2e92b0232838d2004c5a6
ccalinez <ccalinez@hotmail.com>
2025-06-03 00:16:21 -0300

    tp

--- tp2/auction.sol
+++ tp2/auction.sol
@@ -6,22 +6,68 @@ contract Auction {
     address admin;
     string auctioneItem;
     uint256 completionTime;
-    uint minimumBid;
-    uint32 price;
+    uint basePrice;
+
     uint32 extraTime;
+    Bit lastBit;
+
+    mapping(address => Bit) balances;
 
 
-
-
-    constructor(string memory item, uint32 duration, uint32 basePrice){
-        admin = msg.sender;
-        auctioneItem = item;
-        completionTime = block.timestamp + duration;
-        price = basePrice;
+    struct Bit {
+        address owner;
+        uint256 timestamp;
+        uint256 accumulated;
+        uint256 amount;
+        bool exists;
     }
 
-    function bit() external payable {
 
+    constructor(string memory _item, uint32 _duration, uint32 _basePrice){
+        admin = msg.sender;
+        auctioneItem = _item;
+        completionTime = block.timestamp + _duration;
+        basePrice = _basePrice;
+    }
+
+    function bit() external payable active greatter {
+
+
+    }
+
+    function withdrawal() external onlyBidder{
+        
+    }
+
+    // modificador que valida que solo ofertante con deposito pueda retirar el dinero
+    modifier onlyBidder {
+        require(balances[msg.sender].exists, "Only Bidder are allowed.");
+        _;
+    }
+
+    // modificador que verifica que se pueda aplicar una oferta  10 minutos antes que finalice la subasta 
+    // o durante los 10 minutos posteriores de la ultima oferta pasado el tiempo de finalizacion de la subasta
+    modifier active {
+        bool isActive = false;
+        if(block.timestamp < (completionTime - 10 minutes)){
+            isActive = true;
+        }else if(lastBit.exists && block.timestamp < (lastBit.timestamp + 10 minutes)){
+            isActive = true;
+        }
+        require(isActive, "The time for place a bid has finish");
+        _;
+    }
+
+    // modificador que verifica una oferta mayor al 5% de la ultima oferta
+    modifier greatter {
+        bool valid = false;
+        if(lastBit.exists && msg.value > (lastBit.amount + (lastBit.amount * 5) / 100)){
+            valid = true;
+        }else if(msg.value > (basePrice + (basePrice * 5) / 100)){
+            valid = true;
+        }
+        require(valid, "A valid bid must be 5 % greater than the last bid.");
+        _;
     }
 
 }
